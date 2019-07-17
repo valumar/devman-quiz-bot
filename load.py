3cfa39c0ccd2dc536334b3a27f77adc07eb72e97
Petr E. Antonov <AntonovPE@kadastr.ru>
2019-07-17 10:39:18 +0300

    Generating quiz dictionary

--- load.py
+++ load.py
@@ -1,6 +1,8 @@
 import logging
 import zipfile
 import urllib.request
+import re
+import os
 
 from tqdm import tqdm
 
@@ -35,8 +37,43 @@ def extract_zipfile(archive):
         zip_ref.extractall("downloads")
 
 
+def match_data(data):
+    # https://regex101.com/r/UxmySB/2
+    regex = r"(Вопрос \d+:\n)(.*(?:\n(?!Ответ:$).*)*)\n+(Ответ:$)\n(.*(?:\n(?!Вопрос \d+:\n)|(?!Тур:\n).*)*)\n+"
+    matches = re.findall(regex, data, re.MULTILINE)
+    return matches
+
+
+def list_files(folder):
+    files_list = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+    return files_list
+
+
+def add_data_to_dict(title, matches, quiz_dict):
+    quiz_dict[title] = list()
+    for match in matches:
+        quiz_dict[title].append({
+            "Вопрос": match[1],
+            "Ответ": match[3]
+        })
+    return quiz_dict
+
+
+def generate_dict():
+    files_list = list_files('downloads')
+    quiz_dict = dict()
+    for data_file in files_list:
+        file_path = os.path.join('downloads', data_file)
+        with open(file_path, 'r', encoding='koi8-r') as f:
+            data = f.read()
+        matches = match_data(data)
+        title = data.split('\n')[1]
+        add_data_to_dict(title, matches, quiz_dict)
+    return quiz_dict
+
+
 if __name__ == '__main__':
     # r = download_file("http://dvmn.org/media/modules_dist/quiz-questions.zip")
     # extract_zipfile(r)
-    with open('downloads/1vs1200.txt', 'r', encoding='koi8-r') as f:
-        data = f.read()
+    quiz_dict = generate_dict()
+
