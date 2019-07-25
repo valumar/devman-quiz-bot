c9a98928c59ee813ebced867ab6c404aa2d4c079
Petr E. Antonov <AntonovPE@kadastr.ru>
2019-07-25 17:38:45 +0300

    Refactoring.

--- tg_bot.py
+++ tg_bot.py
@@ -1,17 +1,14 @@
 import json
-import random
 import logging
 import os
 import redis
-import pymorphy2
-import requests
 
-from string import punctuation
 from dotenv import load_dotenv
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
                           ConversationHandler)
 
+from text_utils import load_data, get_random_question, spellcheck, compare
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -25,54 +22,6 @@ reply_keyboard = [['Новый вопрос', 'Сдаться'],
                        ['Мой счет']]
 
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-
-
-def load_data(json_file):
-    with open(json_file, 'r') as f:
-        data = json.load(f)
-    return data
-
-
-def get_random_question(data):
-    random_champ = random.choice(list(data.keys()))
-    random_question = random.choice(data[random_champ])
-    random_question['Чампионат'] = random_champ
-    logger.info(f"get_random_question - {random_champ} {random_question}")
-    return random_question
-
-
-def normalize(sentence):
-    morph = pymorphy2.MorphAnalyzer()
-    for character in punctuation:
-        sentence = sentence.replace(character, " ")
-    sentence = sentence.split()
-    normalized_sentence = []
-    for word in sentence:
-        normalized_sentence.append(morph.parse(word)[0].normal_form)
-    return set(normalized_sentence)
-
-
-def compare(answer, reference):
-    normalized_answer = normalize(answer)
-    normalized_reference = normalize(reference)
-    difference = normalized_reference - normalized_answer
-    result = False
-    if len(difference) <= .5 * len(normalized_reference):
-        result = True
-    return result
-
-
-def spellcheck(sentence):
-    url = "https://speller.yandex.net/services/spellservice.json/checkTexts"
-    payload = {
-        "text": sentence
-    }
-    response = requests.get(url, params=payload)
-    if response.ok and len(response.json()[0]) > 0:
-        checked_sentence = " ".join(i['s'][0] for i in response.json()[0])
-        return checked_sentence
-    else:
-        return sentence
 
 
 def start(bot, update):
